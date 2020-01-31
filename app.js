1791efb17ae28a268a55014e2f4f559f0a3b1583
aaronsato <2asato@gmail.com>
2020-01-31 10:45:01 -0800

    updates

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// $(document).ready(function(){
+//
+// /*! Fades in page on load */
+// $('#pic-header').css('display', 'none');
+// // $('#pic-header').fadeIn(5000);
+//
+// });
+
 $(".navbar a").click(function(){
   $("body,html").animate({
    scrollTop:$("#" + $(this).data('value')).offset().top
